44ac2ca4e13b000db7075c87eaffeec7ff417cc2
MirellaDourado <mirellaalvesdourado@gmail.com>
2023-01-27 01:38:15 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+  SELECT 
+	COUNT(h.user_id) AS quantidade_musicas_no_historico
+FROM SpotifyClone.history AS h
+	INNER JOIN SpotifyClone.users AS u
+		ON u.user_id = h.user_id
+WHERE u.user_name = 'Barbara Liskov';
